a67bbb7f9df4933ee9704bf3affa4aa3cd1e1dcb
EsinShadrach <esinshadrachrafe@gmail.com>
2024-04-23 01:35:57 +0100

    initialisation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let current_dir = std::env::current_dir().unwrap();
+
+    let paths = std::fs::read_dir(current_dir).unwrap();
+
+    for path in paths {
+        let entry = path.unwrap();
+        let borrowed_path = entry.path();
+        let current_path = borrowed_path.file_name().unwrap().to_str().unwrap();
+
+        if entry.file_type().unwrap().is_dir() && current_path.starts_with("fudwud") {
+            let output = Command::new("git")
+                .arg("-C")
+                .arg(&current_path)
+                .arg("clean")
+                .arg("-Xfd")
+                .output()
+                .expect("Failed to execute command");
+
+            println!("{}", String::from_utf8_lossy(&output.stdout));
+            println!("{}", String::from_utf8_lossy(&output.stderr));
+        }
+    }
 }
